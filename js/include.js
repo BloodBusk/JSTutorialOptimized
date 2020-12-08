c732daa0c859b7075fa75606f44261df9a7fa5ea
Thomas <thomashbusk@gmail.com>
2020-12-08 22:35:29 +0100

    comments to the include.js code

--- js/include.js
+++ js/include.js
@@ -1,15 +1,24 @@
-window.onload = function () {
-    function a(a, b) {
-        var c = /^(?:file):/,
-            d = new XMLHttpRequest,
-            e = 0;
-        d.onreadystatechange = function () {
-            4 == d.readyState && (e = d.status), c.test(location.href) && d.responseText && (e = 200), 4 == d.readyState && 200 == e && (a.outerHTML = d.responseText)
+/*Include function for multipled implimented html pages inside other html pages*/
+window.onload = () => {
+    let includeHTML = (a, b) => {
+        let c = /^(?:file):/;
+        let d = new XMLHttpRequest;
+        let e = 0;
+        //event handler for readyState changes
+        d.onreadystatechange = () => {
+            //when ready state is 4, operation is complete, this is where we handle the request
+            4 == d.readyState && (e = d.status);
+            c.test(location.href) && d.responseText && (e = 200);
+            4 == d.readyState && 200 == e && (a.outerHTML = d.responseText);
         };
         try {
-            d.open("GET", b, !0), d.send()
-        } catch (f) {}
+            d.open("GET", b, !0); //3 parameters of open(method, url, readState value must not be 0, an unsent state)
+            d.send(); //sends the XMLHttpRequest to server
+        } catch (exc) {}
     }
-    var b, c = document.getElementsByTagName("*");
-    for (b in c) c[b].hasAttribute && c[b].hasAttribute("include-html") && a(c[b], c[b].getAttribute("include-html"))
+    //searches through all tags names and finds the tags with attribute include-html and gets them
+    let c = document.getElementsByTagName("*");
+    for (let b in c) {
+        c[b].hasAttribute && c[b].hasAttribute("include-html") && includeHTML(c[b], c[b].getAttribute("include-html"))
+    }
 };
